c9f82546366386a1a54e72370fd746146b5c7523
buybackoff <vbaybekov@gmail.com>
2019-11-09 20:42:21 +0100

    Use relative path for tools & temp folder

--- dotnet/nuget/local_build_debug.bat
+++ dotnet/nuget/local_build_debug.bat
@@ -1,6 +1,6 @@
 @echo off
 
 dotnet restore ..\src\Spreads.Native\Spreads.Native.csproj
-dotnet pack ..\src\Spreads.Native\Spreads.Native.csproj -c DEBUG -o C:\transient\LocalNuget --no-build -p:AutoSuffix=True
+dotnet pack ..\src\Spreads.Native\Spreads.Native.csproj -c DEBUG -o \transient\LocalNuget --no-build -p:AutoSuffix=True
 
 pause
--- dotnet/nuget/local_build_release.bat
+++ dotnet/nuget/local_build_release.bat
@@ -1,5 +1,5 @@
 @echo off
 
-dotnet pack ..\src\Spreads.Native\Spreads.Native.csproj -c Release -o C:\transient\LocalNuget --no-build -p:AutoSuffix=True
+dotnet pack ..\src\Spreads.Native\Spreads.Native.csproj -c Release -o \transient\LocalNuget --no-build -p:AutoSuffix=True
 
 pause
